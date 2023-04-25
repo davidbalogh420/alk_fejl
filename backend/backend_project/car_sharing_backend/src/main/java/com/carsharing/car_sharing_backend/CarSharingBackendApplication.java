4ed77e111498cfea1382f4de82d9d91a99b07f78
davidbalogh420 <dave.balogh02@gmail.com>
2023-04-25 16:32:40 +0200

    uj projekt letrehozva

---
+++ backend/backend_project/car_sharing_backend/src/main/java/com/carsharing/car_sharing_backend/CarSharingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carsharing.car_sharing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingBackendApplication.class, args);
+    }
+
+}
---
+++ backend/backend_project/car_sharing_backend/src/test/java/com/carsharing/car_sharing_backend/CarSharingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsharing.car_sharing_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
